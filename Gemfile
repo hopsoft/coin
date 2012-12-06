769ddc9564e2d1408b0f2352f8f92f7298ddb46b
Nathan Hopkins <natehop@gmail.com>
2012-12-05 19:55:17 -0700

    Removed code from the gh-pages branch.

--- Gemfile
+++
@@ -1,7 +0,0 @@
-source 'https://rubygems.org'
-
-group :development do
-  gem "micro_test"
-  gem "pry"
-  # gem "pry-stack_explorer"
-end
--- test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-require 'rubygems'
-require 'bundler'
-Bundler.require :default, :development
-require File.join(File.dirname(__FILE__), "..", "lib", "coin")
